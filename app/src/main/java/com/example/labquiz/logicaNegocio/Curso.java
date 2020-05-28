a28869e37362bee1c5038b3d6c99a74af03198c7
DiegoSalazar1998 <55150273+DiegoSalazar1998@users.noreply.github.com>
2020-05-27 18:40:32 -0600

    Initial commit

---
+++ app/src/main/java/com/example/labquiz/logicaNegocio/Curso.java
@@ -0,0 +1,50 @@
+package com.example.labquiz.logicaNegocio;
+
+import java.io.Serializable;
+
+public class Curso implements Serializable {
+    private String idC;
+    private String descripcion;
+    private String creditos;
+
+    public Curso(String idC, String descripcion, String creditos) {
+        this.idC = idC;
+        this.descripcion = descripcion;
+        this.creditos = creditos;
+    }
+
+    public Curso() {
+        this.idC = new String();
+        this.descripcion = new String();
+        this.creditos = new String();
+    }
+
+    public String getIdC() {
+        return idC;
+    }
+
+    public void setIdC(String idC) {
+        this.idC = idC;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(String creditos) {
+        this.creditos = creditos;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" + "idC=" + idC + ", descripcion=" + descripcion + ", creditos=" + creditos + '}';
+    }
+}
---
+++ app/src/main/java/com/example/labquiz/logicaNegocio/Estudiante.java
@@ -0,0 +1,73 @@
+package com.example.labquiz.logicaNegocio;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Estudiante implements Serializable {
+    private String idP;
+    private String nombre;
+    private String apellidos;
+    private String edad;
+    private ArrayList<Curso> cursosAsignados;
+
+    public Estudiante(String idP, String nombre, String apellidos, String edad, ArrayList<Curso> cursosAsignados) {
+        this.idP = idP;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+        this.cursosAsignados = cursosAsignados;
+    }
+
+    public Estudiante() {
+        this.idP = new String();
+        this.nombre = new String();
+        this.apellidos = new String();
+        this.edad = new String();
+        this.cursosAsignados = new ArrayList<>();
+    }
+
+    public String getIdP() {
+        return idP;
+    }
+
+    public void setIdP(String idP) {
+        this.idP = idP;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public ArrayList<Curso> getCursosAsignados() {
+        return cursosAsignados;
+    }
+
+    public void setCursosAsignados(ArrayList<Curso> cursosAsignados) {
+        this.cursosAsignados = cursosAsignados;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" + "idP=" + idP + ", nombre=" + nombre + ", apellidos=" + apellidos + ", edad=" + edad + ", cursosAsignados=" + cursosAsignados + '}';
+    }
+}
